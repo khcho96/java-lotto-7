a82977e694731260672ec95268fb28fc82799b69
khcho96 <khcho1492@gmail.com>
2025-12-02 18:04:15 +0900

    feat: 상수 및 에러메시지 구현

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,11 @@
+package lotto.constant;
+
+public final class Constant {
+
+    public static final int PURCHASE_AMOUNT_UNIT = 1000;
+    public static final int PURCHASE_AMOUNT_MAX = 100_000;
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+}

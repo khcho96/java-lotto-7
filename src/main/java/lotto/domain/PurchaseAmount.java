c53782eaa1f2dd9dff0f28807c38e45f0bd696a5
khcho96 <khcho1492@gmail.com>
2025-12-02 17:25:27 +0900

    feat: 구입 금액 입력값 검증 및 객체 생성

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import static lotto.constant.Constant.PURCHASE_AMOUNT_MAX;
+import static lotto.constant.Constant.PURCHASE_AMOUNT_UNIT;
+import static lotto.constant.ErrorMessage.PURCHASE_AMOUNT_RANGE_ERROR;
+import static lotto.constant.ErrorMessage.PURCHASE_AMOUNT_UNIT_ERROR;
+
+public class PurchaseAmount {
+
+    private final int purchaseAmount;
+
+    private PurchaseAmount(int purchaseAmount) {
+        validateAll(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public static PurchaseAmount from(int purchaseAmount) {
+        return new PurchaseAmount(purchaseAmount);
+    }
+
+    private void validateAll(int purchaseAmount) {
+        validateUnit(purchaseAmount);
+        validateRange(purchaseAmount);
+    }
+
+    private void validateUnit(int purchaseAmount) {
+        if (purchaseAmount % PURCHASE_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_UNIT_ERROR.getErrorMessage());
+        }
+    }
+
+    private void validateRange(int purchaseAmount) {
+        if (purchaseAmount <= 0 || purchaseAmount > PURCHASE_AMOUNT_MAX) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_RANGE_ERROR.getErrorMessage());
+        }
+    }
+}

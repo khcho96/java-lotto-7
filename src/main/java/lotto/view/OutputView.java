ed626663ded1304877ee62a9f35af3ce4a717547
khcho96 <khcho1492@gmail.com>
2025-12-02 19:16:44 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.util.EnumMap;
 import java.util.List;
+import lotto.domain.Rank;
 import lotto.dto.LottoDto;
 import lotto.dto.LottoResultDto;
 
@@ -8,6 +10,8 @@ public class OutputView {
 
     private static final String NEW_LINE = System.lineSeparator();
     private static final String ISSUED_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_RESULT_MESSAGE = "당첨 통계" + NEW_LINE + "---";
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %,.1f%%입니다.";
 
     public static void printLotto(LottoDto lottoDto) {
         List<List<Integer>> lottoNumbers = lottoDto.getLottoNumbers();
@@ -20,8 +24,15 @@ public class OutputView {
     }
 
     public static void printLottoResult(LottoResultDto lottoResultDto) {
-        // TODO: DTO 에서 값 가져오기
-        // TODO: 결과 출력
+        EnumMap<Rank, Integer> result = lottoResultDto.getResult();
+        double profitRate = lottoResultDto.getProfitRate();
+
+        System.out.println(NEW_LINE + LOTTO_RESULT_MESSAGE);
+        for (Rank rank : result.keySet()) {
+            System.out.printf(rank.getPrintMessage() + NEW_LINE, result.get(rank));
+        }
+
+        System.out.println(String.format(PROFIT_RATE_MESSAGE, profitRate));
     }
 
     public static void printErrorMessage(IllegalArgumentException e) {

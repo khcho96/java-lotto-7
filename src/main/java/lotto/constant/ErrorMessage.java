4fcc54dce7079d9fe188f9525cb3ee1793c9e596
khcho96 <khcho1492@gmail.com>
2025-12-02 20:18:27 +0900

    feat: 보너스 번호 검증 상수 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -11,7 +11,9 @@ public enum ErrorMessage {
 
     LOTTO_NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다."),
     LOTTO_NUMBER_RANGE_ERROR("로또 번호는 1 이상 45 이하의 정수여야 합니다."),
-    LOTTO_NUMBER_UNIQUE_ERROR("로또 번호는 중복이 없어야 합니다.");
+    LOTTO_NUMBER_UNIQUE_ERROR("로또 번호는 중복이 없어야 합니다."),
+
+    BONUS_NUMBER_UNIQUE_ERROR("보너스 번호는 당첨 번호와 중복되자 않아야 합니다.");
 
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     private final String errorMessage;

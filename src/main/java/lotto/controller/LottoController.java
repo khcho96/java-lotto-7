1965222d77337036a62ce32419260646b29a89f2
khcho96 <khcho1492@gmail.com>
2025-12-02 18:03:52 +0900

    feat: 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,66 @@
+package lotto.controller;
+
+import lotto.dto.LottoDto;
+import lotto.dto.LottoResultDto;
+import lotto.service.LottoService;
+import lotto.util.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        LottoDto lottoDto = issueLotto(); // DTO 반환 메서드 호출
+        OutputView.printLotto(lottoDto); // DTO 출력
+
+        /*registerWinningNumber(); // 내부 작업만 수행하는 메서드 호출
+        registerBonusNumber();
+
+        LottoResultDto lottoResultDto = getLottoResult(); // 결과 반환 메서드 호출
+        OutputView.printLottoResult(lottoResultDto); // 결과 출력*/
+    }
+
+    private LottoDto issueLotto() {
+        while (true) {
+            try {
+                String rawPurchaseAmount = InputView.readPurchaseAmount();
+                Integer purchaseAmount = InputParser.parseToInteger(rawPurchaseAmount);
+
+                return lottoService.issueLotto(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private void registerWinningNumber() {
+        while (true) {
+            try {
+
+                return;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private void registerBonusNumber() {
+
+    }
+
+    private LottoResultDto getLottoResult() {
+        while (true) {
+            try {
+
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e);
+            }
+        }
+    }
+}
